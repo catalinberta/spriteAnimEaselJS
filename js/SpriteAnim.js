1aa6b0a9b0410aa8c88f95f0feebe2c2a79695f1
Catalin Berta <catalinberta@yahoo.com>
2015-02-04 12:47:45 +0200

    Added onComplete parameter, separated stage from being reinitialized on the same canvas

--- js/SpriteAnim.js
+++ js/SpriteAnim.js
@@ -24,32 +24,40 @@ function SpriteAnim(canvasId) {
 
 SpriteAnim.prototype.init = function(canvasId) {
 	this.canvasId = canvasId;
+	this.stage = new createjs.SpriteStage(this.canvasId);
 }
 
-SpriteAnim.prototype.start = function(imageData,animationName,className) {
+SpriteAnim.prototype.start = function(imageData,animationName,className,runOnComplete) {
 	createjs.Ticker.off("tick", this.animTicker); // Stop any previous Tickers for this instance
+	if(this.stage.getChildAt(0)) { this.stage.removeChildAt(0) } // If child already exists, remove it
+	this.imageData = imageData;
 	this.canvasWidth = imageData.frames.width;
 	this.canvasHeight = imageData.frames.height;
-	this.stage = new createjs.SpriteStage(this.canvasId);
+	this.runOnComplete = runOnComplete;
 	this.stage.canvas.width = this.canvasWidth;
 	this.stage.canvas.height = this.canvasHeight;
 	this.stage.updateViewport(this.canvasWidth,this.canvasHeight);
-	this.imageData = imageData;
-	this.spritesheet = new createjs.SpriteSheet(this.imageData);
-	this.animation = new createjs.Sprite(this.spritesheet);
-	this.container = this.animation;
-	this.stage.addChild(this.container);
-	this.animationName = animationName;
-	this.animation.gotoAndPlay(this.animationName);
 	this.animTicker = createjs.Ticker.on("tick", this.stage);
 	this.className = className;
+	this.spritesheet = new createjs.SpriteSheet(this.imageData);
+	this.animation = new createjs.Sprite(this.spritesheet);
+	this.stage.addChild(this.animation);
+	this.animationName = animationName;
+	this.animation.gotoAndPlay(this.animationName);
 
 	if(this.className) {
 		this.stage.canvas.className = this.stage.canvas.className + ' ' + this.className;
 	}
 
+	if(this.runOnComplete) {
+		this.animation.on('animationend', this.onComplete)
+	}
 }
 
 SpriteAnim.prototype.stop = function() {
 	createjs.Ticker.off("tick", this.animTicker);
+}
+
+SpriteAnim.prototype.onComplete = function() {
+	// On complete code here
 }
--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 // EaselJS Sprite Sheets
 // SpriteSheet Documentation http://createjs.com/Docs/EaselJS/classes/SpriteSheet.html
-var imageData = {
+imageData = {
     images: ['/img/car.jpg'], // Image element from PreloadJS LoadQueue
     frames: {width:126, height:126}, // Frame sizes
     framerate: 30, // Spritesheet framerate
     animations: {
-        run : [0,120,true]
+    	run : [0,90,true]
     }
 };
-var imageData2 = {
-    images: ['/img/horse.png'],
-    frames: {width:128, height:128},
-    framerate: 20,
-    animations: {
-        run : { frames : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-        			next:true
-    			}
-    }
+imageData2 = {
+	images: ['/img/horse.png'],
+	frames: {width:128, height:128},
+	framerate: 20,
+	animations: {
+		run : { frames : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+			next:true
+		}
+	}
 };
+
 
 //on ready
 $(function() {
@@ -27,10 +28,10 @@ $(function() {
 
 	//first canvas actions
 	$('.canvas_1 input.start_1').click(function() {
-		firstCanvas.start(imageData,'run',false);
+		firstCanvas.start(imageData,'run',false,true);
 	});
 	$('.canvas_1 input.start_2').click(function() {
-		firstCanvas.start(imageData2,'run',false);
+		firstCanvas.start(imageData2,'run',false,false);
 	});
 	$('.canvas_1 input.stop').click(function() {
 		firstCanvas.stop();
@@ -39,10 +40,10 @@ $(function() {
 
 	//second canvas actions
 	$('.canvas_2 input.start_1').click(function() {
-		secondCanvas.start(imageData,'run',false);
+		secondCanvas.start(imageData,'run',false,false);
 	});
 	$('.canvas_2 input.start_2').click(function() {
-		secondCanvas.start(imageData2,'run',false);
+		secondCanvas.start(imageData2,'run',false,true);
 	});
 	$('.canvas_2 input.stop').click(function() {
 		secondCanvas.stop();
